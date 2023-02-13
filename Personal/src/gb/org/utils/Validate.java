aa8468d65a8f91f2c612e540d3281489d018eb70
Nikos-I <109852894+Nikos-I@users.noreply.github.com>
2023-02-13 23:48:41 +0300

    Seminar5 Personal finish commit

--- Personal/src/gb/org/utils/Validate.java
+++ Personal/src/gb/org/utils/Validate.java
@@ -4,7 +4,7 @@ public class Validate {
 
     public void checkNumber(String telephone) throws Exception {
         if(!telephone.substring(0,1).equals("+")) {
-            throw new PhoneException("Ошибка телефон начинается с плюсика");
+            throw new PhoneException("Ошибка - телефон должен начинаться с плюса");
         }
         else if(telephone.length() != 12) {
             throw new PhoneException("Длина телефона должна быть 11");
